e5c56fdf1eac60af3dd5a88d000f0a508da89b01
Robin <robin.b@aximsoft.com>
2019-03-11 10:11:49 +0530

    Updates

---
+++ precache-manifest.4e26f26ec88abfa0228f1df41a822eca.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "77c3f78cd98c009fa688",
+    "url": "/burger/static/css/4.0f28f63d.chunk.css"
+  },
+  {
+    "revision": "2dd0988b94570f75f82d",
+    "url": "/burger/static/css/main.ed0555d6.chunk.css"
+  },
+  {
+    "revision": "a048e3ae6908e7242554",
+    "url": "/burger/static/js/runtime~main.a048e3ae.js"
+  },
+  {
+    "revision": "5cd9f31cdb16c6b4b095",
+    "url": "/burger/static/js/2.5cd9f31c.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "8d385f554a9df64706a3",
+    "url": "/burger/static/js/3.8d385f55.chunk.js"
+  },
+  {
+    "revision": "2dd0988b94570f75f82d",
+    "url": "/burger/static/js/main.2dd0988b.chunk.js"
+  },
+  {
+    "revision": "77c3f78cd98c009fa688",
+    "url": "/burger/static/js/4.77c3f78c.chunk.js"
+  },
+  {
+    "revision": "c1eceea41a3b7bc5ed8e",
+    "url": "/burger/static/js/5.c1eceea4.chunk.js"
+  },
+  {
+    "revision": "c1eceea41a3b7bc5ed8e",
+    "url": "/burger/static/css/5.cda058bb.chunk.css"
+  },
+  {
+    "revision": "8d385f554a9df64706a3",
+    "url": "/burger/static/css/3.3572193d.chunk.css"
+  },
+  {
+    "revision": "f820d7925102dc5681e022dc00c259c1",
+    "url": "/burger/index.html"
+  }
+];
